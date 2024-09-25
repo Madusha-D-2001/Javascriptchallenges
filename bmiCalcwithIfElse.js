c86ba80b4d5048f11f93aa518ed26b6dfeef73e7
Madusha Dasanayaka <113464476+Madusha-D-2001@users.noreply.github.com>
2024-09-25 15:10:36 +0530

    Add files via upload

---
+++ bmiCalcwithIfElse.js
@@ -0,0 +1,24 @@
+function bmiCalculator (weight, height) {
+  var interpretation = Math.floor(weight / (height*height));
+  
+  
+
+  if(interpretation<18.5){
+       return  "Your BMI is "+interpretation+", so you are underweight." ;
+  }
+ else if (interpretation > 18.5 && interpretation <= 24.9){
+
+      return  "Your BMI is "+interpretation+", so you have a normal weight." ;
+
+  }
+
+  else if (interpretation > 24.9){
+
+      return  "Your BMI is "+interpretation+", so you are overweight." ;
+
+  }
+
+  return interpretation;
+
+}
+  
