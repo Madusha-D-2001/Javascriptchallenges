3e1a697528b2533bb4237e4bc276c1e61a93320f
Madusha Dasanayaka <113464476+Madusha-D-2001@users.noreply.github.com>
2024-09-25 11:04:44 +0530

    Add files via upload

---
+++ bmiCalculator.js
@@ -0,0 +1,10 @@
+
+
+function bmiCalculator(weight,height){
+    
+  var bmi = Math.floor(weight / (height*height));
+  return bmi;
+}
+var bmi = bmiCalculator(65, 1.8); 
+console.log("Your BMI is:"+ " " + bmi);
+
